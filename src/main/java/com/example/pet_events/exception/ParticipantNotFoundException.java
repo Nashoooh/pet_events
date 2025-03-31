e7088e322b298dc53cd1d212d3f5adc96fa032a7
Ignacio Andana De Lapyera <Ignacioad92@gmail.com>
2025-03-31 18:36:06 -0300

    [UPDATE] elimina excepcion sin usar

--- src/main/java/com/example/pet_events/exception/ParticipantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.pet_events.exception;
-
-public class ParticipantNotFoundException extends RuntimeException {
-    public ParticipantNotFoundException(String message) {
-        super(message);
-    }
-}
